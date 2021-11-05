8457e9f9c66955f4b990dbd524a450925eecb3f0
Mark Konakov <konakovmark@yandex.ru>
2021-11-05 13:00:58 +0300

    Get vacancies from Moscow area

--- main.py
+++ main.py
@@ -6,9 +6,13 @@ hh_header = {
     'User-Agent': 'salary_benchmarking_app/1.0 konakov-dev@yandex.ru'
 }
 params = {
-    'specialization': '1.221'
+    'specialization': '1.221',
+    'area': '1',
+    'period': '300',
+    'per_page': '100',
 }
 
 response = requests.get(hh_api_url, headers=hh_header, params=params)
 response.raise_for_status()
-print(response.json())
+amount_vacancies = response.json()['found']
+print(amount_vacancies)
